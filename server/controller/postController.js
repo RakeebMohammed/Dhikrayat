23ac443cab4cde3abe0257bf8b0e8747b35783ab
RakeebMohammed <rkbmohd@gmail.com>
2023-07-05 17:08:27 +0530

    post delete and like completed

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -1,32 +1,55 @@
-const { default: mongoose } = require('mongoose')
-let postSchema=require('../modal/postSchema')
+const { default: mongoose } = require("mongoose");
+let postSchema = require("../modal/postSchema");
 
-exports.getPosts=async(req,res)=>{
-      
-        let result=await postSchema.find()
-res.status(201).json(result)
-}
-exports.createPost=async(req,res)=>{
-        console.log(req.body);
- let newPost= new postSchema(req.body)
- newPost=await newPost.save()
- console.log(newPost);
- res.status(201).json(newPost)
-}
-exports.getPost=async(req,res)=>{
-console.log(req.body);
-        const {id}=req.body
-        let result=await postSchema.find({_id:id})
-        console.log(result);    
+exports.getPosts = async (req, res) => {
+  let result = await postSchema.find();
+  result.reverse()
+  res.status(201).json(result);
+};
+exports.createPost = async (req, res) => {
+  console.log(req.body);
+  let newPost = new postSchema(req.body);
+  newPost = await newPost.save();
+  console.log(newPost);
+  res.status(201).json(newPost);
+};
+exports.getPost = async (req, res) => {
+  console.log(req.body);
+  const { id } = req.body;
+  let result = await postSchema.find({ _id: id });
+  console.log(result);
+};
+exports.updatePost = async (req, res) => {
+  const { id } = req.params;
 
-}
-exports.updatePost=async(req,res)=>{
-       const {id}=req.params
-       
-        console.log(req.body);
-       
-        if(!mongoose.Types.ObjectId.isValid(id))return res.status(404).send('no post with that id')
-        let updated=await postSchema.findByIdAndUpdate({_id:id},req.body,{new:true})
-       res.status(200).json(updated) 
-       console.log(updated);
-}
+  console.log(req.body);
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("no post with that id");
+  let updated = await postSchema.findByIdAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
+  res.status(200).json(updated);
+  
+};
+exports.deletePost = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("no post with that id");
+  await postSchema.deleteOne({ _id: id });
+  res.status(200).json({message:"Deleted one post"});
+};
+exports.likePost = async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send("no post with that id");
+    const post=await postSchema.findById({_id:id})
+    
+    let updated = await postSchema.findByIdAndUpdate({ _id: id }, {likeCount:post.likeCount?+post.likeCount+1:1}, {
+      new: true,
+    });
+    res.status(200).json(updated);
+    console.log(updated);
+};
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,9 +1,11 @@
 let express=require('express')
 let router=express.Router()
-let {getPosts,createPost,getPost,updatePost}=require('../controller/postController')
+let {getPosts,createPost,getPost,updatePost,deletePost, likePost}=require('../controller/postController')
 router.get('/',getPosts)
 router.post('/createPost',createPost)
 router.patch('/:id',updatePost)
+router.delete('/:id',deletePost)
+router.put('/:id',likePost)
 router.get('/getPost',getPost)
 
 module.exports=router
