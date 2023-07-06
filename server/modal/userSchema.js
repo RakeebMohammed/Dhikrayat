107a5c568057007c0fc679458c06d4dcac97c877
RakeebMohammed <rkbmohd@gmail.com>
2023-07-07 04:26:27 +0530

    clean the code

--- server/modal/userSchema.js
+++ server/modal/userSchema.js
@@ -3,8 +3,8 @@ const schema=mongoose.Schema({
     firstname:String,
     lastname:String,
     password:String,
-    email:String
-
+    email:String,
+ name:String
 })
 const userSchema=mongoose.model('Users',schema)
 module.exports=userSchema
