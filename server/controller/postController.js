1a45b633fa06f5a6754d2f36c93060074addd9dd
RakeebMohammed <rkbmohd@gmail.com>
2023-07-12 21:50:32 +0530

    styling started

--- server/controller/postController.js
+++ server/controller/postController.js
@@ -2,15 +2,24 @@ const { default: mongoose } = require("mongoose");
 let postSchema = require("../modal/postSchema");
 
 exports.getPosts = async (req, res) => {
- 
-  let {page}=req.query
-  page=parseInt(page)
-  const limit =2
-  const currentIndex=(page-1)*limit
-const total=await postSchema.count()
-  let posts = await postSchema.find().sort({_id:-1}).limit(limit).skip(currentIndex);
- console.log(page);
-  res.status(201).json({data:posts,currentPage:page,totalPage:Math.ceil(total/limit)});
+  let { page } = req.query;
+  page = parseInt(page);
+  const limit = 2;
+  const currentIndex = (page - 1) * limit;
+  const total = await postSchema.count();
+  let posts = await postSchema
+    .find()
+    .sort({ _id: -1 })
+    .limit(limit)
+    .skip(currentIndex);
+  console.log(page);
+  res
+    .status(201)
+    .json({
+      data: posts,
+      currentPage: page,
+      totalPage: Math.ceil(total / limit),
+    });
 };
 exports.createPost = async (req, res) => {
   console.log(req.body);
@@ -20,10 +29,10 @@ exports.createPost = async (req, res) => {
   res.status(201).json(newPost);
 };
 exports.getPost = async (req, res) => {
- console.log(req.params);
+  console.log(req.params);
   const { id } = req.params;
   let post = await postSchema.find({ _id: id });
-  res.status(200).json(post)
+  res.status(200).json(post);
 };
 exports.updatePost = async (req, res) => {
   const { id } = req.params;
@@ -36,7 +45,6 @@ exports.updatePost = async (req, res) => {
     new: true,
   });
   res.status(200).json(updated);
-  
 };
 exports.deletePost = async (req, res) => {
   const { id } = req.params;
@@ -44,32 +52,34 @@ exports.deletePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("no post with that id");
   await postSchema.deleteOne({ _id: id });
-  res.status(200).json({message:"Deleted one post"});
+  res.status(200).json({ message: "Deleted one post" });
 };
 exports.likePost = async (req, res) => {
   const { id } = req.params;
   console.log(id);
-  if(!req.userId) return res.json({message:"Unauthenticated access"})
+  if (!req.userId) return res.json({ message: "Unauthenticated access" });
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("no post with that id");
-    const post=await postSchema.findById({_id:id})
+  const post = await postSchema.findById({ _id: id });
 
-    const index=post.likes.filter(like=>like===req.userId)
-    !index.length>0?
-    post.likes.push(req.userId):post.likes.filter(like=>like!==req.userId)
-    
-    let updated = await postSchema.findByIdAndUpdate({ _id: id }, post, {
-      new: true,
-    });
-    res.status(200).json(updated);
-    console.log(updated);
+  const index = post.likes.filter((like) => like === req.userId);
+  !index.length > 0
+    ? post.likes.push(req.userId)
+    : post.likes.filter((like) => like !== req.userId);
+
+  let updated = await postSchema.findByIdAndUpdate({ _id: id }, post, {
+    new: true,
+  });
+  res.status(200).json(updated);
+  console.log(updated);
 };
-exports.searchPosts=async(req,res)=>{
- const {query,tags}=req.query
-console.log(req.query);
-const title=new RegExp(query,'i')
-const posts=await postSchema.find({$or:[{title},{tags:{$in:tags.split(',')}}]})
-console.log(posts);
-res.status(200).json(posts)
-}
-
+exports.searchPosts = async (req, res) => {
+  const { query, tags } = req.query;
+  console.log(req.query);
+  const title = new RegExp(query, "i");
+  const posts = await postSchema.find({
+    $or: [{ title }, { tags: { $in: tags.split(",") } }],
+  });
+  console.log(posts);
+  res.status(200).json(posts);
+};
