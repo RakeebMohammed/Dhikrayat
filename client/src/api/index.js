2e5b6510d8649f55b765d28e26170af858f11733
RakeebMohammed <rkbmohd@gmail.com>
2023-07-25 01:21:39 +0530

    base url changed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({ baseURL: "http://localhost:3001" });
+const API = axios.create({ baseURL: "https://memories-lp8k.onrender.com/" });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("user")) {
     req.headers.Authorization = `Bearer ${
